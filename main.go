0a41aa85e9aa4d9d7e7f79773114e2266db4f09c
DuXiaolong <326582310@qq.com>
2019-11-07 15:24:48 +0800

    add multi function

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
-package gitTest
+package main
+
+import (
+	"fmt"
+	"gitTest/calc"
+)
+
+func main()  {
+	fmt.Println("calc.Add called!")
+	res:=calc.Add(10,20)
+	fmt.Println("res:",res)
+	fmt.Println("calc.Sub called!")
+	res=calc.Sub(10,20)
+	fmt.Println("res:",res)
+	fmt.Println("calc.Multi called!")
+	res=calc.Multi(10,20)
+	fmt.Println("10*20=:",res)
+}
